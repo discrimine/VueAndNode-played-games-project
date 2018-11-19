34546203b5c399fce6f28156cb122696dbf916a1
ahulyuk <ahulyuk@vivasg.com>
2018-11-19 16:59:47 +0200

    part of page structure

---
+++ src/components/Games.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="games-blocks">
+    <div class="games-block">
+      <div class="games-block_title"> in process </div>
+      <div class="games-block_games">
+        <div class="games-block_game" v-for="game in games" :key="game.id">
+          {{ game.name }}
+        </div>
+      </div> 
+    </div>
+    <div class="games-block">
+      <div class="games-block_title"> completed </div>
+    </div>
+    <div class="games-block">
+     <div class="games-block_title"> not yet </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  private games = [
+    {
+      id: 1,
+      name: 'name',
+    },
+    {
+      id: 2,
+      name: 'name2',
+    },
+    {
+      id: 2,
+      name: 'name2',
+    },
+  ];
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.games-blocks {
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+  justify-content: space-between;
+  max-width: 1440px;
+  .games-block {
+    border: 1px solid #696968;
+    width: 32%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    .games-block_title {
+      margin-top: 15px;
+      font-weight: 400;
+      font-size: 28px;
+      width: 100%;
+    }
+    .games-block_games {
+      margin-top: 30px;
+      display: flex;
+      flex-direction: row;
+      .games-block_game {
+      width: 50px;
+      height: 50px;
+      margin: 0 25px;
+      border: 1px solid #19530d;
+      }
+    }
+  }
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<games></games>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Games from '@/components/Games.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Games,
   },
 })
 export default class Home extends Vue {}
