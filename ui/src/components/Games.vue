1dd68800b37847abbb770cfa0f87cb613c020974
discrimine <discrimine96@gmail.com>
2018-11-23 21:49:12 +0200

    working prototype with get games, and add game methods

--- ui/src/components/Games.vue
+++ ui/src/components/Games.vue
@@ -8,189 +8,99 @@
         <div class="header_controls">
           <div class="header_controls-container">
             <div class="header_controls__design"></div>
-            <div class="header_controls__buttons"></div>
+            <div class="header_controls__buttons">
+                <b-form-select v-model="newGameEntity.genre" class="mb-3">
+                    <option value="Action">Action</option>
+                    <option value="Shooter">Shooter</option>
+                    <option value="Arcade">Arcade</option>
+                    <option value="RPG">RPG</option>
+                    <option value="Strategy">Strategy</option>
+                    <option value="Platformer">Platformer</option>
+                </b-form-select>
+                <b-form-select v-model="newGameEntity.status" class="mb-3">
+                    <option value="0">not yet</option>
+                    <option value="1">completed</option>
+                    <option value="2">in process</option>
+                </b-form-select>
+                 <b-form-input v-model="newGameEntity.name"
+                  type="text"
+                  placeholder="game title">
+                </b-form-input>
+              <b-button v-on:click="$emit('addGame')" :variant="'primary'"> add new </b-button>
+            </div>
           </div>
         </div>
       </div>
+      <b-button v-on:click="$emit('getData')"> get </b-button>
       <div class="games-blocks col-12">
-        <div class="games-block col-3">
-          <div class="games-block_title"> in process </div>
-          <div class="games-block_games">
-            <div class="games-block_game" v-for="game in games" :key="game.id">
-              {{ game.name }}
-            </div>
-          </div> 
-        </div>
-        <div class="games-block col-3">
-          <div class="games-block_title"> completed </div>
-        </div>
-        <div class="games-block col-3">
-        <div class="games-block_title"> not yet </div>
-        </div>
-      </div>
-    </div>
+        <div class="games-block col-3" v-for="gameStatus in gamesView" :key="gameStatus.title">
+          <div class="games-block_title"> {{ gameStatus.title }} </div>
+          <div class="games-block_genres" v-for="(genre, title) in gameStatus.genres" :key="genre.id">
+            <div class="games-block_genre">
+              <div class="games-block_genre__title">
+                {{ title }}
+              </div>
+              <div class="games-block_genre__body" v-for="game in genre" :key="game.id">
+                <div class="games-block_genre__body___game">
+                  {{ game.name }}
+                </div>
+              </div>
+            </div>  
+          </div>
+        </div> 
+      </div> 
+     </div>  
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-@Component
+import axios from 'axios';
 
-export type TIGamesResponseItem = {
-  id: number,
-  name: string,
-  genre: string,
-  status: (0 | 1 | 2)
-};
+export interface TIGamesResponseItem {
+  id: number;
+  name: string;
+  genre: string;
+  status: (0 | 1 | 2);
+}
+
+export interface TIGameAdd  {
+  name: string | undefined;
+  genre: string | undefined;
+  status: ((0 | 1 | 2 | undefined));
+}
+
 export type TIGamesResponseReturn = TIGamesResponseItem[];
 
-export type TICortegeGames = {
+export interface TICortegeGames {
   0: {
     title: 'not yet',
     genres: {
 
-    }
-  }
+    },
+  };
   1: {
     title: 'completed',
     genres: {
 
-    }
-  }
+    },
+  };
   2: {
     title: 'in process',
     genres: {
 
-    }
-  }
+    },
+  };
 }
-
-const games: TIGamesResponseReturn = [
-    {
-      id: 1,
-      name: 'Kingdom Come: Deliverance',
-      genre: 'RPG',
-      status: 2
-    },
-    {
-      id: 2,
-      name: 'Metro',
-      genre: 'shooter',
-      status: 0
-    },
-    {
-      id: 3,
-      name: 'Dying Light',
-      genre: 'shooter',
-      status: 0
-    },
-    {
-      id: 4,
-      name: 'Deus Ex',
-      genre: 'shooter',
-      status: 0
-    },
-    {
-      id: 5,
-      name: 'fallout 3',
-      genre: 'RPG',
-      status: 0
-    },
-    {
-      id: 6,
-      name: 'titan quest',
-      genre: 'RPG',
-      status: 0
-    },
-    {
-      id: 7,
-      name: 'darksouls3',
-      genre: 'RPG',
-      status: 1
-    },
-    {
-      id: 8,
-      name: 'wither1',
-      genre: 'RPG',
-      status: 1
-    },
-    {
-      id: 9,
-      name: 'Skyrim',
-      genre: 'RPG',
-      status: 1
-    },
-    {
-      id: 10,
-      name: 'GTA 5',
-      genre: 'shooter',
-      status: 1
-    },
-    {
-      id: 11,
-      name: 'GTA 4',
-      genre: 'shooter',
-      status: 1
-    },
-    {
-      id: 12,
-      name: 'Far Cry 5',
-      genre: 'shooter',
-      status: 1
-    },
-    {
-      id: 13,
-      name: 'Time shift',
-      genre: 'shooter',
-      status: 1
-    },
-  ];
-//TODO complete data method
-  function returnUniqueFromObj(arr: TIGamesResponseReturn) {
-    let obj = {};
-    for (let i = 0; i < arr.length; i++) {
-      let status: string = arr[i].status;
-      let genre: string = arr[i].genre;
-      obj[String(genre) + String(status)] = true;
-    }
-
-    return obj;
-  }
-  const uniqStatuses = returnUniqueFromObj(games)
-  const gamesLength = games.length;
-  console.log('1', uniqStatuses);
-  let cortegeGames: TICortegeGames = {
-    0: {
-      title: 'not yet',
-      genres: [
-
-      ]
-    },
-    1: {
-      title: 'completed',
-      genres: [
-
-      ]
-    },
-    2: {
-      title: 'in process',
-      genres: [
-
-      ]
-    }
-  }
-
-  for (let i: number = 0; i < uniqStatusesLength; i++) {
-  }
-
-console.log(cortegeGames);
-
-
+@Component
 export default class Games extends Vue {
+@Prop() private gamesView;
+@Prop() private newGameEntity;
 
-  
+public games: TIGamesResponseReturn = [];
 
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -211,15 +121,11 @@ export default class Games extends Vue {
       font-size: 28px;
       width: 100%;
     }
-    .games-block_games {
-      margin-top: 30px;
-      display: flex;
-      flex-direction: row;
-      .games-block_game {
-        width: 50px;
-        height: 50px;
-        margin: 0px 15px;
-        border: 1px solid #19530d;
+    .games-block_genres{
+      .games-block_genre{
+        .games-block_genre__title {
+          font-weight: 600
+        }
       }
     }
   }
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,14 +1,112 @@
 <template>
-<games></games>
+<Games
+  v-on:getData="getData()"
+  v-on:addGame="addGame()"
+  :gamesView="gamesView"
+  :newGameEntity="newGameEntity"
+  >
+  </Games>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Games from '@/components/Games.vue'; // @ is an alias to /src
+import axios from 'axios';
+
 @Component({
   components: {
     Games,
   },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+   
+  },
+  
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+  public gamesView: any = {
+    0: {
+      title: 'not yet',
+      genres: {},
+    },
+    1: {
+      title: 'completed',
+      genres: {},
+    },
+    2: {
+      title: 'in process',
+      genres: {},
+    },
+  }
+
+  public getData() {
+  axios.get('http://localhost:3000/games/')
+    .then((json) => {
+      const response = json.data;
+      const games_length = response.length;
+      for (let i = 0; i < games_length; i++) {
+        const status: number = response[i].status;
+        const genre: string = response[i].genre;
+        Vue.set(this.gamesView[status].genres, genre, []);
+      }
+
+      for (const game of json.data) {
+        this.gamesView[game.status].genres[game.genre].push({
+          name: game.name,
+        });
+      }
+    });
+  }
+
+  public newGameEntity: any = {
+    name: undefined,
+    genre: undefined,
+    status: undefined,
+  };
+
+  public addGame() {
+    const newGame = this.newGameEntity;
+    let newGameResponse: any = [];
+    let validation = true;
+
+    for (const gameAttribute in newGame) {
+      if (newGame[gameAttribute] === undefined) {
+        validation = false;
+      } else {
+        newGameResponse.push({
+          propName: gameAttribute,
+          value: newGame[gameAttribute],
+        });
+      }
+    }
+
+    newGameResponse = JSON.stringify(newGameResponse);
+
+    if (!validation) {
+      alert('choose game attributes');
+    } else {
+      fetch('http://localhost:3000/games/', {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: newGameResponse,
+      })
+      .then((response) => {
+        this.getData();
+        
+        return response.json();
+      });
+    }
+  }
+
+  created() {
+    this.getData();
+  }
+}
 </script>
